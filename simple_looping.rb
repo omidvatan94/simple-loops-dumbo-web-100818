b71d54a1112548d44dbcd77f0f2eb5fecd410c53
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-23 18:39:41 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,8 @@ def loop_iterator(number_of_times)
   loop do 
   number_of_times += 1 
   puts "Welcome to Flatiron School's Web Development Course!"
-  if number_of_times 
+  if number_of_times >=7
+    break
 end
 end
 
