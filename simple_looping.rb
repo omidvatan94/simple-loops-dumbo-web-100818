fbc520c8ebe2572d3eb637d404f5f5661546e518
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-23 18:27:46 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -9,7 +9,7 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
+  7.times do 
   puts "Welcome to Flatiron School's Web Development Course!"
   
 end
