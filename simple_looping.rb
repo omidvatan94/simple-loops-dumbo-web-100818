cb8f539133667bc0b2c7c5bfcfe8d5ec748997c4
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-23 18:29:09 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -25,7 +25,7 @@ def until_iterator(number_of_times)
   counter = 0 
   while counter < 7 
   puts "Welcome to Flatiron School's Web Development Course!"
-
+  counter 
 end
 
 def for_iterator(number_of_times)
